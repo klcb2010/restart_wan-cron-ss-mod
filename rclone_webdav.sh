19e15ab249fcf6c0699a0b9724ed0e94b965d477
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2026-01-03 20:46:04 +0800

    Update rclone_webdav.sh

--- rclone_webdav.sh
+++ rclone_webdav.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# 等待网络、证书、硬盘完全就绪（你路由器 60-90 秒够用）
+# 等待网络、证书、硬盘完全就绪
 sleep 90
 
 # 防止重复启动
 if ! pgrep -f "rclone serve webdav" > /dev/null; then
     nohup /opt/bin/rclone serve webdav /tmp/mnt/SD/ --addr :8080 \
-    --user 英文用户名 --pass 字母数字密码 \
+    --user 字母数字用户名 --pass 字母数字密码 \
     --cert /etc/cert.pem --key /etc/key.pem \
     > /tmp/rclone.log 2>&1 &
     logger "rclone WebDAV (HTTPS) started successfully"
